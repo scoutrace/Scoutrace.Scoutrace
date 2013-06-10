d0f10a332acd9441265d0a90de935355bbd121aa
Soren Malling <soren.malling@gmail.com>
2013-06-10 12:34:56 +0200

    [TASK] Initial commit of models and test files

---
+++ Classes/Scoutrace/Scoutrace/Domain/Model/Team.php
@@ -0,0 +1,19 @@
+<?php
+namespace Scoutrace\Scoutrace\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Scoutrace.Scoutrace".   *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @Flow\Entity
+ */
+class Team {
+
+
+}
+?>

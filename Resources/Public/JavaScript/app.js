8ea2edca5263151b796d1fe6aa0d095a2f823220
Znegl <mail@thomasgrinderslev.dk>
2013-10-13 17:23:07 +0200

    Moved front end libraries again and implemented basic layout

--- Resources/Public/JavaScript/app.js
+++ Resources/Public/JavaScript/app.js
@@ -1,15 +1,23 @@
 // Set up paths for libraries
 require.config({
 	paths: {
-		'jquery': 'Libraries/jquery',
-		'ember': 'Libraries/ember',
-		'bootstrap': 'Libraries/bootstrap',
-		'handlebars': 'Libraries/handlebars',
-		'leaflet': 'Libraries/leaflet'
+		'jquery': '../Libraries/jQuery/jquery',
+		'ember': '../Libraries/Ember/ember',
+		'bootstrap': '../Libraries/bootstrap/js/bootstrap.min',
+		'handlebars': '../Libraries/Handlebars/handlebars',
+		'leaflet': '../Libraries/Leaflet/leaflet'
+	},
+	shim: {
+		'bootstrap': {
+			deps: ['jquery']
+		},
+		'ember': {
+			deps: ['jquery', 'handlebars']
+		}
 	}
 });
 
 // Require libraries and run application
 require(['jquery', 'ember', 'bootstrap', 'handlebars', 'leaflet'], function(jQuery, Ember, Bootstrap, Handlebars, Leaflet) {
-	alert('yæi!');
+	//alert('yæi!');
 });
